589b5416f9497292fb88f195294a7b1b4e2f76ad
vedant Yevatikar <96629752+vedantBCodes@users.noreply.github.com>
2023-08-08 15:26:51 +0530

    new program added

---
+++ hollowInvertedFullPyramid.cpp
@@ -0,0 +1,49 @@
+/*
+* * * * *
+ *     *
+  *   *
+   * *
+    *
+*/
+#include<iostream>
+using namespace std;
+ 
+ void hollowInvertedFullPyramid(int n)
+ {
+    int col=n*2-1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            if(i==1)
+            {
+            if(j%2!=0)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+            }
+            else if(j==i || j==(col+1-i))
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+ }
+
+int main()
+{
+    int row=9;
+    // cout<<"Enter the number of rows:";
+    // cin>>row;
+    hollowInvertedFullPyramid(row);
+   return 0;
+}
