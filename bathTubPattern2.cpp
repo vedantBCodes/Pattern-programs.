8fbb563c97cd0e3e9ccc920b82210b4c9bea6755
vedant Yeotikar <bichkundevedant97@gmail.com>
2024-10-10 23:26:14 +0530

    added another approach with less time complexity

--- bathTubPattern2.cpp
+++ bathTubPattern2.cpp
@@ -22,10 +22,31 @@ void bathTubPattern(int row)
     }
     
 }
+void bathTubPattern2(int row)
+{
+    int col=row*2;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            if(j>=1 && j<=i || j>(col-i) && j<=col)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<"\n";
+    }
+}
 
 int main()
 {
     int row=10;
     bathTubPattern(row);
+    cout<<endl;
+    bathTubPattern2(row);
    return 0;
 }
