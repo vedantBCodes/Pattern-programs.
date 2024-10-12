2bc4ebe9680fde82edb51fa6407b116eb2532c17
vedant Yeotikar <bichkundevedant97@gmail.com>
2024-10-12 11:49:43 +0530

    added another approach with less time complexity

--- hollowHourglassPattern.cpp
+++ hollowHourglassPattern.cpp
@@ -17,7 +17,7 @@
 #include <iostream>
 using namespace std;
 
-void hollowHourglassPattern(int n)
+void hollowHourglassPattern(int n)  //Bad approach
 {
      int size=(n*2)-1; //Here size is the number of horizontal rows
     for(int i=1;i<=size;i++)
@@ -62,8 +62,34 @@ void hollowHourglassPattern(int n)
         cout<<endl;
     }
 }
+
+void hollowHourglassPattern2(int row)  //Best approach
+{
+    int col=row;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            if((i==1 && j%2==1) || (i==row && j%2==1)|| (i!=1 && j==i) || (i!=1 && j==col+1-i))
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
 int main() {
     int n=10; //Here n is the number of stars in the first horizontal row;
     hollowHourglassPattern(n);
+    cout<<endl;
+ if(n%2==0)
+ {
+  n++;
+ }
+    hollowHourglassPattern(n) //Here n is the number of rows in Hollow Hourglass Pattern and it should be an odd number
     return 0;
 }
