872e692d4857ba0c5d04462f68862c68e4ee412d
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-10-30 13:04:21 +0530

    Update diamondPattern2.cpp

--- diamondPattern2.cpp
+++ diamondPattern2.cpp
@@ -35,11 +35,31 @@ void diamondPattern(int row)
     }    
 }
 
+void diamondPattern2(int row)
+{
+    int col=row/2+1;
+   for(int i=1;i<=row;i++)
+   {
+     for(int j=1;j<=col;j++)
+     {
+        if(((j>col-i) && i<=col) || ((i>col) && (j>i-col)) )
+        {
+            cout<<" *";
+        }
+        else
+        {
+            cout<<" ";
+        }
+     }
+     cout<<endl;
+   }
+}
+
 int main()
 {
     int row;
     cout<<"Enter the number of rows:";
     cin>>row;
-    diamondPattern(row);
+    diamondPattern(row); //Use odd numbers only
    return 0;
 }
